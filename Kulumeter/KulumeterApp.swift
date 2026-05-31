aef0d64d134ee7ba59df7a1273c004952f285911
Rasmus Sten <rasmus@dll.nu>
2026-05-31 18:00:44 +0300

    Initial Commit

---
+++ Kulumeter/KulumeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  KulumeterApp.swift
+//  Kulumeter
+//
+//  Created by Rasmus Sten on 31.5.2026.
+//
+
+import SwiftUI
+
+@main
+struct KulumeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KulumeterTests/KulumeterTests.swift
@@ -0,0 +1,19 @@
+//
+//  KulumeterTests.swift
+//  KulumeterTests
+//
+//  Created by Rasmus Sten on 31.5.2026.
+//
+
+import Testing
+@testable import Kulumeter
+
+struct KulumeterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
